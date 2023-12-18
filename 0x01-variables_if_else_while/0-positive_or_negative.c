4514ae933bfd52247eb0b681335537e12b2ba03b
Lanceky <evanslance100@gmail.com>
2023-12-18 15:12:14 -0800

    0-positive_or_negative.c exe code

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int n;
+
+    /* Initialize seed for random number generation */
+    srand(time(0));
+
+    /* Generate a random number and assign it to n */
+    n = rand() - RAND_MAX / 2;
+
+    /* Print the random number */
+    printf("The number %d is ", n);
+
+    /* Check if the number is positive, negative, or zero */
+    if (n > 0)
+        printf("positive\n");
+    else if (n == 0)
+        printf("zero\n");
+    else
+        printf("negative\n");
+
+    return (0);
+}
+
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This dir has info about 0x01. C - Variables, if, else, while
