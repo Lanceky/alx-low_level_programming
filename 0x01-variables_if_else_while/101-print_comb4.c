4a52d4b8d21478625d11b50ba2443496287f7286
Lanceky <evanslance100@gmail.com>
2023-12-18 17:05:05 -0800

    c10 code 1

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of three digits.
+ * Numbers must be separated by ", " followed by a space.
+ * The three digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination
+ * of the three digits 0, 1, and 2.
+ * Print only the smallest combination of three digits.
+ * Numbers should be printed in ascending order, with three digits.
+ * You can only use the putchar function (every other function is forbidden).
+ * You can only use putchar six times maximum in your code.
+ * You are not allowed to use any variable of type char.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int i, j, k;
@@ -29,6 +45,5 @@ int main(void)
     /* Print a newline at the end */
     putchar('\n');
 
-    return 0;
+    return (0);
 }
-
