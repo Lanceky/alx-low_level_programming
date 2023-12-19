4a5e7788aedff5cc12b3e608da78dfa67369eebf
Lanceky <evanslance100@gmail.com>
2023-12-19 14:25:50 -0800

    perfect cf10 code1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: The first integer
+ * @b: The second integer
+ *
+ * Return: The result of the addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
