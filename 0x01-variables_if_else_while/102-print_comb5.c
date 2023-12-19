65773fa3cf270cd5be6a6ac99f9dac90c2bc4e18
Lanceky <evanslance100@gmail.com>
2023-12-18 17:27:37 -0800

    c11 code 3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,28 +18,28 @@
  */
 int main(void)
 {
-	int num1, num2;
+    int num1, num2;
 
-	for (num1 = 0; num1 < 99; ++num1)
-	{
-		for (num2 = num1 + 1; num2 < 100; ++num2)
-		{
-			putchar((num1 / 10) + '0');
-			putchar((num1 % 10) + '0');
-			putchar(' ');
-			putchar((num2 / 10) + '0');
-			putchar((num2 % 10) + '0');
+    for (num1 = 0; num1 < 99; ++num1)
+    {
+        for (num2 = num1 + 1; num2 < 100; ++num2)
+        {
+            putchar((num1 / 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar(' ');
+            putchar((num2 / 10) + '0');
+            putchar((num2 % 10) + '0');
 
-			if (num1 < 98 || num2 < 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+            if (num1 < 98 || num2 < 99)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
 
