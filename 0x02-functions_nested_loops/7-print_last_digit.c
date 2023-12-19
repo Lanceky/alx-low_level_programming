52e3b19c268f1bc655bad3c20ea069c26c292859
Lanceky <evanslance100@gmail.com>
2023-12-19 14:12:02 -0800

    almost perfect cf7 code1

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,15 +8,15 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    last_digit = n % 10;
+	last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-    _putchar('0' + last_digit);
+	_putchar('0' + last_digit);
 
-    return (last_digit);
+	return (last_digit);
 }
 
