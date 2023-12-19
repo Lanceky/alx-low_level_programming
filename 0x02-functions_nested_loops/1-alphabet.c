1add3a27f37af722577aab0d9a42d7b794f92276
Lanceky <evanslance100@gmail.com>
2023-12-19 12:53:50 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase, followed by a new line.
+ */
+void print_alphabet(void)
+{
+	/* Using _putchar to print each character of the alphabet */
+	_putchar('a');
+	_putchar('b');
+	_putchar('c');
+	_putchar('d');
+	_putchar('e');
+	_putchar('f');
+	_putchar('g');
+	_putchar('h');
+	_putchar('i');
+	_putchar('j');
+	_putchar('k');
+	_putchar('l');
+	_putchar('m');
+	_putchar('n');
+	_putchar('o');
+	_putchar('p');
+	_putchar('q');
+	_putchar('r');
+	_putchar('s');
+	_putchar('t');
+	_putchar('u');
+	_putchar('v');
+	_putchar('w');
+	_putchar('x');
+	_putchar('y');
+	_putchar('z');
+
+	/* Printing a new line at the end */
+	_putchar('\n');
+}
