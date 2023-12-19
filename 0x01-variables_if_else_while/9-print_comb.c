7bdd11d81be5bb4fd9e3da6f1a8825aac51df0ce
Lanceky <evanslance100@gmail.com>
2023-12-18 16:28:33 -0800

    c9 code 2

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,20 +7,20 @@
  */
 int main(void)
 {
-	int digit;
+    int digit;
 
-	for (digit = 0; digit < 10; digit++)
-	{
-		putchar(digit + '0');
-		if (digit < 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+    for (digit = 0; digit < 10; digit++)
+    {
+        putchar(digit + '0');
+        if (digit < 9)
+        {
+            putchar(',');
+            putchar(' ');
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
 
