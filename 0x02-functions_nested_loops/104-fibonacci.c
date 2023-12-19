b6ff479c929044e4656fbb0f102e9379ddaaf168
Lanceky <evanslance100@gmail.com>
2023-12-19 14:46:35 -0800

    cf16 code1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the first 98 Fibonacci numbers
+ *              separated by commas
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     int count;
@@ -7,7 +15,7 @@ int main(void)
 
     printf("%lu, %lu", first, second);
 
-    for (count = 2; count < 98; ++count)
+    for (count = 3; count <= 98; ++count)
     {
         next = first + second;
         printf(", %lu", next);
