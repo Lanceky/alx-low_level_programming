cf154893a61a1f4c248a830fa919dc0d988118b6
Lanceky <evanslance100@gmail.com>
2023-12-21 14:55:54 -0800

    10print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,30 +1,31 @@
 #include "main.h"
 
 /**
- * print_triangle - prints a triangle
- * @size: size of the triangle
+ * print_triangle - Prints a triangle of a given size.
+ * @size: The size of the triangle.
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
-    if (size <= 0)
-    {
-        _putchar('\n');
-        return;
-    }
+	int i, j;
 
-    int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            if (j < size - i - 1)
-                _putchar(' ');
-            else
-                _putchar('#');
-        }
-
-        _putchar('\n');
-    }
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j < size - i - 1)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
 }
 
