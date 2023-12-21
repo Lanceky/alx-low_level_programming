9303d88c80496ae0740f51797cfb76af94572840
Lanceky <evanslance100@gmail.com>
2023-12-21 14:04:46 -0800

    more FUNCTIONS5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,19 +5,19 @@
  */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j <= 14; j++)
-        {
-            if (j > 9)
-            {
-                _putchar('0' + (j / 10));
-            }
-            _putchar('0' + (j % 10));
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar('0' + (j / 10));
+			}
+			_putchar('0' + (j % 10));
+		}
+		_putchar('\n');
+	}
 }
 
