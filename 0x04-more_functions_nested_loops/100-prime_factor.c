8f8446c631475bebbdc2c94fd07e75b169df8509
Lanceky <evanslance100@gmail.com>
2023-12-21 14:50:06 -0800

    100primefactor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,36 +9,41 @@
  */
 long largest_prime_factor(long n)
 {
-    long i;
+	long i;
 
-    while (n % 2 == 0)
-    {
-        n = n / 2;
-    }
+	while (n % 2 == 0)
+	{
+		n = n / 2;
+	}
 
-    for (i = 3; i <= sqrt(n); i = i + 2)
-    {
-        while (n % i == 0)
-        {
-            n = n / i;
-        }
-    }
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			n = n / i;
+		}
+	}
 
-    if (n > 2)
-    {
-        return n;
-    }
+	if (n > 2)
+	{
+		return n;
+	}
 
-    return i;
+	return i;
 }
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    long number = 612852475143;
-    long result = largest_prime_factor(number);
+	long number = 612852475143;
+	long result = largest_prime_factor(number);
 
-    printf("%ld\n", result);
+	printf("%ld\n", result);
 
-    return 0;
+	return 0;
 }
 
