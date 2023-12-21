fa4329d5a036feefde5fb85f8dc907351bb87a45
Lanceky <evanslance100@gmail.com>
2023-12-21 14:11:27 -0800

    more FUNCTIONS6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,15 +6,18 @@
  */
 void print_line(int n)
 {
-    int i;
+	if (n > 0)
+	{
+		int i;
 
-    if (n > 0)
-    {
-        for (i = 0; i < n; i++)
-        {
-            _putchar('_');
-        }
-    }
-    _putchar('\n');
+		for (i = 0; i < n; i++)
+			_putchar('_');
+
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
 
