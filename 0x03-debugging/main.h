be593f06edc8e76e18f7ce36ac6f32fc5a42c4ff
Lanceky <evanslance100@gmail.com>
2023-12-22 09:44:16 -0800

    2d final

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,12 +1,9 @@
+
 #ifndef MAIN_H
 #define MAIN_H
 int   putchar();
 int main(void);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-void print_remaining_days(int month, int day, int year);
-int largest_number(int a, int b, int c);
+
 #endif /* MAIN_H */
 
