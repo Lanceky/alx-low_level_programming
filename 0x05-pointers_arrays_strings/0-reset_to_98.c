f3d0b85299f28f748e083ae5363d66212356bf73
Lanceky <evanslance100@gmail.com>
2024-01-02 08:26:10 -0800

    pointers adn arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value a pointer points to 98.
+ * @n: Pointer to an integer to be updated.
+ */
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains all tasks for 0x05-pointers_arrays_strings
