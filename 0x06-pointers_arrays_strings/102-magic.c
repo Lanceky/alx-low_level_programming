1968585285ddee496968db685ac66d5651af875a
Lanceky <evanslance100@gmail.com>
2024-01-06 03:02:16 -0800

    102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,16 +2,20 @@
 
 int main(void)
 {
-    int a[5];
-    int *p;
+	int a[5];
+	int *p;
 
-    a[2] = 102;
-    p = &a[2] - 2;
+	/* Initialize array element at index 2 */
+	a[2] = 102;
+	/* Set pointer p to address of a[2] - 2 */
+	p = &a[2] - 2;
 
-    /* Your code goes here */
-    *(p + 5) = 98;
+	/* Your code goes here */
+	/* Update the value at the calculated memory location (a[2]) to 98 */
+	*(p + 5) = 98;
 
-    printf("a[2] = %d\n", a[2]);
-    return (0);
+	/* Print the updated value of a[2] */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
 
