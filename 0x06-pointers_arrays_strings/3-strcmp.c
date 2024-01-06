e8c82dc3541abc049ed550897f4016ca5e29be86
Lanceky <evanslance100@gmail.com>
2024-01-06 02:27:38 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
+	int diff;
+
 	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
 	}
 
-	return *(unsigned char *)s1 - *(unsigned char *)s2;
+	diff = *(unsigned char *)s1 - *(unsigned char *)s2;
+	return (diff);
 }
 
