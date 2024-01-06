57deb3e90407e244f94dc9240b141f10cea89838
Lanceky <evanslance100@gmail.com>
2024-01-05 18:48:58 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,27 +5,28 @@
  * @dest: The destination string.
  * @src: The source string.
  *
- * Return: A pointer to the resulting string `dest`.
+ * Return: A pointer to the resulting string dest.
  */
 char *_strcat(char *dest, char *src)
 {
-    char *result = dest;
+	char *result = dest;
 
-    /* Move to the end of dest */
-    while (*dest)
-        dest++;
+	/* Move to the end of dest */
+	while (*dest)
+		dest++;
 
-    /* Copy src to dest */
-    while (*src)
-    {
-        *dest = *src;
-        dest++;
-        src++;
-    }
+	/* Copy src to dest */
+	while (*src)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
 
-    /* Add null terminator at the end */
-    *dest = '\0';
+	/* Add null terminator at the end */
+	*dest = '\0';
 
-    return (result);
+	return (result);
 }
+
 
