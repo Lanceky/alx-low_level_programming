83ead4f4f290c48bc4a594e355c47ff7c94608d1
Lanceky <evanslance100@gmail.com>
2024-01-05 19:25:10 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,23 +11,23 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
 
 int main(void)
 {
-    char s1[] = "Hello";
-    char s2[] = "World!";
+	char s1[] = "Hello";
+	char s2[] = "World!";
 
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
 }
 
