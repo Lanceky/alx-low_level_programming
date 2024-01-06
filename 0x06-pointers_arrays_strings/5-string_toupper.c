88007296e6137a96bfef6362ff0ca33179fa4fbd
Lanceky <evanslance100@gmail.com>
2024-01-06 02:30:45 -0800

    5-sting tupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * string_toupper - Converts all lowercase letters of a string to uppercase.
- * @str: The string to convert.
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: Input string.
  *
  * Return: Pointer to the resulting string.
  */
@@ -14,21 +13,10 @@ char *string_toupper(char *str)
 	while (*ptr)
 	{
 		if (*ptr >= 'a' && *ptr <= 'z')
-			*ptr -= ('a' - 'A');
+			*ptr = *ptr - 'a' + 'A';
 		ptr++;
 	}
 
 	return (str);
-}
-
-int main(void)
-{
-	char str[] = "Look up!\n";
-	char *ptr;
-
-	ptr = string_toupper(str);
-	printf("%s", ptr);
-	printf("%s", str);
-	return (0);
 }
 
