c7e80f11eeb46c5c2901258907b545291905e53a
Lanceky <evanslance100@gmail.com>
2024-01-06 02:18:41 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,33 +1,33 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * _strcmp - Compares two strings.
- * @s1: The first string.
- * @s2: The second string.
+ * _strcmp - Compare two strings.
+ * @s1: First string.
+ * @s2: Second string.
  *
- * Return: Integer less than, equal to, or greater than zero if s1 is found,
- * respectively, to be less than, to match, or be greater than s2.
+ * Return: Negative if s1 is less than s2,
+ *         Positive if s1 is greater than s2,
+ *         0 if s1 is equal to s2.
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && (*s1 == *s2))
-	{
-		s1++;
-		s2++;
-	}
+    while (*s1 && (*s1 == *s2))
+    {
+        s1++;
+        s2++;
+    }
 
-	return (*(unsigned char *)s1 - *(unsigned char *)s2);
+    return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
 int main(void)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+    char s1[] = "Hello";
+    char s2[] = "World!";
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
 }
 
