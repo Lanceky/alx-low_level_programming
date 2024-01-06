50ff6f29f7c312de96595cbaaf98b13967e75691
Lanceky <evanslance100@gmail.com>
2024-01-06 02:24:20 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,12 +11,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    while (*s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-    }
+	while (*s1 && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
 
-    return *(unsigned char *)s1 - *(unsigned char *)s2;
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
 
