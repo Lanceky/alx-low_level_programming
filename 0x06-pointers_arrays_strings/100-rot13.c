0e876cd3863a537158a35b297f645e82378a9067
Lanceky <evanslance100@gmail.com>
2024-01-06 02:45:35 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,24 +2,30 @@
 
 char rot13_table[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+/**
+ * rot13 - Encodes a string using rot13.
+ * @str: Input string.
+ *
+ * Return: Pointer to the resulting string.
+ */
 char *rot13(char *str)
 {
-    char *ptr = str;
-    int i;
+	char *ptr = str;
+	int i;
 
-    while (*ptr)
-    {
-        for (i = 0; (i < 52) && ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z')); i++)
-        {
-            if ((*ptr == rot13_table[i]) || (*ptr == rot13_table[i] + 32))
-            {
-                *ptr = (i < 26) ? rot13_table[i + 26] : rot13_table[i - 26];
-                break;
-            }
-        }
-        ptr++;
-    }
+	while (*ptr)
+	{
+		for (i = 0; (i < 52) && ((*ptr >= 'A' && *ptr <= 'Z') || (*ptr >= 'a' && *ptr <= 'z')); i++)
+		{
+			if ((*ptr == rot13_table[i]) || (*ptr == rot13_table[i] + 32))
+			{
+				*ptr = (i < 26) ? rot13_table[i + 26] : rot13_table[i - 26];
+				break;
+			}
+		}
+		ptr++;
+	}
 
-    return (str);
+	return (str);
 }
 
