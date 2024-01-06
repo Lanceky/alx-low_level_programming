e41b9585beec2a4d6097512cbf998303694d9a21
Lanceky <evanslance100@gmail.com>
2024-01-05 19:07:42 -0800

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337.
+ * @str: The input string.
+ *
+ * Return: A pointer to the resulting string.
+ */
+char *leet(char *str)
+{
+	int i, j;
+	char leetMap[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char leetReplace[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; leetMap[j] != '\0'; j++)
+		{
+			if (str[i] == leetMap[j])
+			{
+				str[i] = leetReplace[j];
+				break;
+			}
+		}
+	}
+
+	return str;
+}
+
