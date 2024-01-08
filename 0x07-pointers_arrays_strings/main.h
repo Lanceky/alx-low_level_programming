d15ebac79e4b0c75066118b5f6a52399911a93a4
Lanceky <evanslance100@gmail.com>
2024-01-08 11:47:31 -0800

    7print chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,6 +6,8 @@
 char *_memset(char *s, char b, unsigned int n);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_strchr(char *s, char c);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
