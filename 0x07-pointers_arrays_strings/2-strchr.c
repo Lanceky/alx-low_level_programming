2e193cda305871fa3b6606781a862aa0d2aa3d21
Lanceky <evanslance100@gmail.com>
2024-01-08 10:14:09 -0800

    2c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return s;
+		s++;
+	}
 
-    if (*s == c)
-        return s;
+	if (*s == c)
+		return s;
 
-    return (NULL);
+	return (NULL);
 }
 
