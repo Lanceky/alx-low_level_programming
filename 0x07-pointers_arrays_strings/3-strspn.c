4a871e1386d9b14016261c8aacba41667f82fe3b
Lanceky <evanslance100@gmail.com>
2024-01-09 13:17:45 -0800

    7ptr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,23 +10,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int count = 0;
-    int i, j;
+	unsigned int count = 0;
+	int i, j;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                count++;
-                break;
-            }
-        }
-        if (accept[j] == '\0')
-            break;
-    }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
 
-    return (count);
+	return (count);
 }
 
