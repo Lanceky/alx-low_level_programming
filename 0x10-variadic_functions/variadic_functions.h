48e1b3d642cf4191ec7546fb14dc01f244961cc5
Lanceky <evanslance100@gmail.com>
2024-01-25 11:33:08 -0800

    1-print_numbers.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,6 +5,9 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
 
