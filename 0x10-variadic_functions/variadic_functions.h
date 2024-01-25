f252c50cd618b0a9f95ab881daefd323c8044b83
Lanceky <evanslance100@gmail.com>
2024-01-25 11:38:10 -0800

    2-print_string.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,6 +8,6 @@ int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif /* VARIADIC_FUNCTIONS_H */
 
