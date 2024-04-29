021122449d93000e46103afaf116e4a5ed1549fa
Lanceky <evanslance100@gmail.com>
2024-04-28 21:53:57 -0700

    removed indentation

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,12 +6,13 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0')
-        return;
-    
-    /* Move to the next character recursively */
-    _print_rev_recursion(s + 1); 
-    
-    /* Print the current character */
-    _putchar(*s); 
+	if (*s == '\0')
+		return;
+
+	/* Move to the next character recursively */
+	_print_rev_recursion(s + 1);
+
+	/* Print the current character */
+	_putchar(*s);
+}
 }
