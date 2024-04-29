c07bf19248f03a76037edfeff0f85df2a896307b
Lanceky <evanslance100@gmail.com>
2024-04-28 22:01:28 -0700

    2-strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,10 +8,10 @@
  */
 int _strlen_recursion(char *s)
 {
-    if (*s == '\0')
-        return 0;
+	if (*s == '\0')
+		return (0);
 
-    /* Recursively call _strlen_recursion with the next character */
-    return 1 + _strlen_recursion(s + 1);
+	/* Recursively call _strlen_recursion with the next character */
+	return (1 + _strlen_recursion(s + 1));
 }
 
