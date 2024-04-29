1171ca7f97d857623f8378cb3007374b5c762997
Lanceky <evanslance100@gmail.com>
2024-04-28 21:26:48 -0700

    removed indentation

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -15,13 +15,13 @@ int _putchar(char c);
  */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-        _putchar('\n');
-        return;
-    }
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
 
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 
