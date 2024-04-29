221b9fbcc0576c13fb9c51a1a4ae24df3ae3721b
Lanceky <evanslance100@gmail.com>
2024-04-28 21:47:37 -0700

    1-print

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse recursively
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+        return;
+    
+    _print_rev_recursion(s + 1); // Move to the next character recursively
+    _putchar(*s); // Print the current character
+}
+
