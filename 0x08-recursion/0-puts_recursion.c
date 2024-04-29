8084550cc6ad3bfd0db13adf4c8e2f4bcc3f283e
Lanceky <evanslance100@gmail.com>
2024-04-28 21:32:11 -0700

    removed indentaion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c);
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: the string to print
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
