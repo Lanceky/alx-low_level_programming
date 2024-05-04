c9b03b466455439302b0d2424136e640af002879
Lanceky <evanslance100@gmail.com>
2024-05-03 22:54:49 -0700

    wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,21 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0') /* Both strings reached the end simultaneously */
-		return (1);
+    if (*s1 == '\0' && *s2 == '\0') /* Both strings reached the end simultaneously */
+        return (1);
 
-	if (*s2 == '*') /* If s2 contains a '*' */
-	{
-		if (*s1 == '\0') /* If s1 is also at the end, skip over the '*' */
-			return (wildcmp(s1, s2 + 1));
-		if ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1))) /* Try to match with or without consuming a character from s1 */
-			return (1);
-		return (0); /* If neither case matches, return 0 */
-	}
+    if (*s2 == '*') /* If s2 contains a '*' */
+    {
+        if (*s1 == '\0') /* If s1 is also at the end, skip over the '*' */
+            return (wildcmp(s1, s2 + 1));
+        if ((wildcmp(s1 + 1, s2)) || (wildcmp(s1, s2 + 1))) /* Try to match with or without consuming a character from s1 */
+            return (1);
+        return (0); /* If neither case matches, return 0 */
+    }
 
-	if (*s1 != *s2) /* If characters do not match */
-		return (0);
+    if (*s1 != *s2) /* If characters do not match */
+        return (0);
 
-	return (wildcmp(s1 + 1, s2 + 1)); /* Move to the next characters in both strings */
+    return (wildcmp(s1 + 1, s2 + 1)); /* Move to the next characters in both strings */
 }
 
