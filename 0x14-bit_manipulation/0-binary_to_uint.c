cdbd268c512532ed5d7645cd3d13a352a0d1c6e5
Lanceky <evanslance100@gmail.com>
2024-06-09 06:05:08 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,17 +13,17 @@ unsigned int binary_to_uint(const char *b)
     unsigned int result = 0;
 
     if (b == NULL)
-        return 0;
+        return (0);
 
     while (*b)
     {
         if (*b != '0' && *b != '1')
-            return 0;
+            return (0);
 
         result = (result * 2) + (*b - '0');
         b++;
     }
 
-    return result;
+    return (result);
 }
 
