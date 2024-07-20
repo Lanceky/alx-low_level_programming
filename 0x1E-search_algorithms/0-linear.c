62deb21892258262dc6ddfbc5ceddc60522afa20
Lanceky <evanslance100@gmail.com>
2024-07-20 11:41:46 -0700

    added

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,39 +1,27 @@
 #include <stddef.h>  // For size_t
-#include <stdlib.h>  // For malloc, free
+#include "search_algos.h"
 
 /**
  * linear_search - Searches for a value in an array of integers using Linear search algorithm.
  * @array: Pointer to the first element of the array to search in.
  * @size: Number of elements in the array.
  * @value: The value to search for in the array.
- * @log: Pointer to a string buffer where search log will be stored.
  *
  * Return: The first index where value is located, or -1 if not found or array is NULL.
  */
-int linear_search(int *array, size_t size, int value, char **log)
+int linear_search(int *array, size_t size, int value)
 {
     if (array == NULL)
         return -1;
 
-    *log = malloc(size * 30);  // Allocate memory for log (approx. 30 chars per element)
-    if (*log == NULL)
-        return -1;
-
-    **log = '\0';  // Initialize log as empty string
-
     for (size_t i = 0; i < size; i++)
     {
-        if (i > 0)
-            strcat(*log, " ");  // Add space separator between entries
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
         
-        char temp[30];
-        sprintf(temp, "Value checked array[%lu] = [%d]", i, array[i]);
-        strcat(*log, temp);  // Append current comparison to log
-
         if (array[i] == value)
-            return i;  // Return index if value found
+            return i;  // Return the index where the value is found
     }
 
-    return -1;  // Value not found
+    return -1;  // Return -1 if the value is not found
 }
 
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,8 +3,8 @@
 
 #include <stddef.h>  // For size_t
 
-/* Function prototypes */
-int linear_search(int *array, size_t size, int value, char **log);
+/* Function prototype */
+int linear_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
 
