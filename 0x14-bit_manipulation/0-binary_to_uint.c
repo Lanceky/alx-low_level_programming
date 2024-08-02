353395e16dc57635cf771277243f0ae6b085f5c7
Lance <evanslance100@gmail.com>
2024-08-02 02:29:30 -0700

    0-binary-to_uint.cs

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: binary string to convert
- *
- * Return: the converted number, or 0 if:
- *         - there is one or more chars in the string b that is not 0 or 1
- *         - b is NULL
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string of 0 and 1 characters.
+ * 
+ * Return: The converted number, or 0 if there is an invalid character or b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
     unsigned int result = 0;
+    int i = 0;
 
     if (b == NULL)
         return (0);
 
-    while (*b)
+    while (b[i] != '\0')
     {
-        if (*b != '0' && *b != '1')
+        if (b[i] != '0' && b[i] != '1')
             return (0);
 
-        result = (result * 2) + (*b - '0');
-        b++;
+        result = (result << 1) | (b[i] - '0');
+        i++;
     }
 
     return (result);
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>  // For NULL
+
+/* Function prototypes for bit manipulation tasks */
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+/* Prototype for _putchar function, typically used for output */
+int _putchar(char c);
 
 #endif /* MAIN_H */
+
 
