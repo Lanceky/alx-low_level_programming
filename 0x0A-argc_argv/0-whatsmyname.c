df53f3cc95f6255457e6fdaa8894922c6104e20f
Lance <evanslance100@gmail.com>
2024-08-04 11:21:07 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * 
+ * Return: Always 0 (Success).
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc;  // Ignore unused parameter warning
+    printf("%s\n", argv[0]);
+    return 0;
+}
+
