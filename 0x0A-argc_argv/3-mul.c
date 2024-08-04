625ce4030aed00e45732ffa6471143d4ad78496e
Lance <evanslance100@gmail.com>
2024-08-04 11:45:39 -0700

    added mulc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - Prints all command-line arguments.
+ * main - Multiplies two numbers and prints the result.
  * @argc: Number of command-line arguments.
  * @argv: Array of command-line arguments.
  * 
- * Return: Always 0 (Success).
+ * Return: 0 if successful, 1 if error.
  */
 int main(int argc, char *argv[])
 {
-int i;
+int num1, num2, result;
 
-for (i = 0; i < argc; i++)
+if (argc != 3)
 {
-printf("%s\n", argv[i]);
+printf("Error\n");
+return (1);
 }
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+result = num1 * num2;
+
+printf("%d\n", result);
 return (0);
 }
 
