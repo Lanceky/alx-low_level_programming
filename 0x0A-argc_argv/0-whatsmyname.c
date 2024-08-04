1678e3519a72b89167dff3150642c716d7d180a4
Lance <evanslance100@gmail.com>
2024-08-04 11:31:47 -0700

    added main.h

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,8 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-    (void)argc;  /* Ignore unused parameter warning */
-    printf("%s\n", argv[0]);
-    return (0);
+(void)argc;  /* Ignore unused parameter warning */
+printf("%s\n", argv[0]);
+return (0);
 }
 
