3746ac21f55b8ca1f4e69af169abf3b447feb321
Lance <evanslance100@gmail.com>
2024-08-05 21:47:28 -0700

    added 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,25 +10,25 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *array;
-unsigned int i;
+    char *array;
+    unsigned int i;
 
-if (size == 0)
-{
-return (NULL);
-}
+    if (size == 0)
+    {
+        return (NULL);
+    }
 
-array = (char *)malloc(size * sizeof(char));
-if (array == NULL)
-{
-return (NULL);
-}
+    array = (char *)malloc(size * sizeof(char));
+    if (array == NULL)
+    {
+        return (NULL);
+    }
 
-for (i = 0; i < size; i++)
-{
-array[i] = c;
-}
+    for (i = 0; i < size; i++)
+    {
+        array[i] = c;
+    }
 
-return (array);
+    return (array);
 }
 
