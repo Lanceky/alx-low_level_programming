56f66d4d40d094073aebfad3c75c3d7635f42f6e
Lance <evanslance100@gmail.com>
2024-08-05 21:49:08 -0700

    removed spaces at start of newline

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,33 +2,33 @@
 #include <stdlib.h>
 
 /**
- * create_array - creates an array of chars, initialized with a specific char.
- * @size: the size of the array to create
- * @c: the char to initialize each element of the array
- *
- * Return: pointer to the array, or NULL if size is 0 or memory allocation fails
- */
+* create_array - creates an array of chars, initialized with a specific char.
+* @size: the size of the array to create
+* @c: the char to initialize each element of the array
+*
+* Return: pointer to the array, or NULL if size is 0 or memory allocation fails
+*/
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+char *array;
+unsigned int i;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+if (size == 0)
+{
+return (NULL);
+}
 
-    array = (char *)malloc(size * sizeof(char));
-    if (array == NULL)
-    {
-        return (NULL);
-    }
+array = (char *)malloc(size * sizeof(char));
+if (array == NULL)
+{
+return (NULL);
+}
 
-    for (i = 0; i < size; i++)
-    {
-        array[i] = c;
-    }
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
 
-    return (array);
+return (array);
 }
 
