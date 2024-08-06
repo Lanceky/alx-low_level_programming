28e9c1b289454e9cdf5d1a0e36cf27fee1563d69
Lance <evanslance100@gmail.com>
2024-08-05 21:59:52 -0700

    addded prototypes to header file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -20,31 +20,57 @@ return (len);
 }
 
 /**
-* count_words - counts the number of words in a string.
-* @str: the string to check
+* skip_spaces - skips spaces in a string.
+* @str: the string to process
 *
-* Return: number of words
+* Return: pointer to the first non-space character
 */
-int count_words(char *str)
-{
-int words = 0;
-
-while (*str)
+char *skip_spaces(char *str)
 {
 while (*str == ' ')
 {
 str++;
 }
 
-if (*str)
+return (str);
+}
+
+/**
+* allocate_words - allocates memory for the words in the string.
+* @str: the string to split
+* @word_count: number of words
+*
+* Return: pointer to the array of strings, or NULL on failure
+*/
+char **allocate_words(char *str, int word_count)
 {
-words++;
-while (*str && *str != ' ')
+char **words;
+int i, len;
+
+words = (char **)malloc((word_count + 1) * sizeof(char *));
+if (words == NULL)
 {
-str++;
+return (NULL);
 }
+
+for (i = 0; i < word_count; i++)
+{
+str = skip_spaces(str);
+len = word_len(str);
+words[i] = (char *)malloc((len + 1) * sizeof(char));
+if (words[i] == NULL)
+{
+for (int j = 0; j < i; j++)
+{
+free(words[j]);
 }
+free(words);
+return (NULL);
 }
+str += len;
+}
+
+words[word_count] = NULL;
 
 return (words);
 }
@@ -58,7 +84,7 @@ return (words);
 char **strtow(char *str)
 {
 char **words;
-int i, w, len, word_count;
+int i, w, word_count;
 
 if (str == NULL || *str == '\0')
 {
@@ -71,7 +97,7 @@ if (word_count == 0)
 return (NULL);
 }
 
-words = (char **)malloc((word_count + 1) * sizeof(char *));
+words = allocate_words(str, word_count);
 if (words == NULL)
 {
 return (NULL);
@@ -80,25 +106,10 @@ return (NULL);
 i = 0;
 while (*str)
 {
-while (*str == ' ')
-{
-str++;
-}
-
+str = skip_spaces(str);
 if (*str)
 {
-len = word_len(str);
-words[i] = (char *)malloc((len + 1) * sizeof(char));
-if (words[i] == NULL)
-{
-for (w = 0; w < i; w++)
-{
-free(words[w]);
-}
-free(words);
-return (NULL);
-}
-
+word_len(str);
 for (w = 0; w < len; w++)
 {
 words[i][w] = str[w];
@@ -108,8 +119,6 @@ i++;
 str += len;
 }
 }
-
-words[i] = NULL;
 
 return (words);
 }
