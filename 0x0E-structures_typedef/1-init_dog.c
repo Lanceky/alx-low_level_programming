ef8280e0a7a46469d1e14cd9782e76b2128094b8
Lance <evanslance100@gmail.com>
2024-08-08 09:06:57 -0700

    added stddef.h as header file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,20 +1,21 @@
+/* 1-init_dog.c */
+#include <stddef.h>  // For NULL
 #include "dog.h"
-#include <stdlib.h>  /* Include to define NULL */
 
 /**
- * init_dog - Initializes a variable of type struct dog
- * @d: Pointer to the struct dog to initialize
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * init_dog - Initializes a dog structure
+ * @d: Pointer to a dog structure
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-    if (d == NULL)
-        return;
-
-    d->name = name;
-    d->age = age;
-    d->owner = owner;
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
 }
 
