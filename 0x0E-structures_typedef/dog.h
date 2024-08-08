fd3d935df08260db7b0e166284893dfa333f78e5
Lance <evanslance100@gmail.com>
2024-08-08 09:01:31 -0700

    revert to original

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,8 +2,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stddef.h>  // For NULL
-
 /**
  * struct dog - A dog's basic info
  * @name: The dog's name
@@ -18,14 +16,6 @@ struct dog
     float age;
     char *owner;
 };
-
-typedef struct dog dog_t;
-
-/* Function prototypes */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif /* DOG_H */
 
