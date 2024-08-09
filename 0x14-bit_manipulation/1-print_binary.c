079bc531eebb79d2eb1b1e4df7c34ea17c18a83c
Lance <evanslance100@gmail.com>
2024-08-09 03:39:32 -0700

    removed whitespace

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,14 +2,14 @@
 
 /**
  * print_binary - prints the binary representation of a number
- * @n: number to print
+ * @n: number to be printed
  */
 void print_binary(unsigned long int n)
 {
-    if (n >> 1)
-    {
-        print_binary(n >> 1);
-    }
-    _putchar((n & 1) + '0');
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar((n & 1) + '0');
 }
 
