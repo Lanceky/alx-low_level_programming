5f48cd187d98ca381341d5ed677e7a32f4925b65
Lance <evanslance100@gmail.com>
2024-08-09 03:28:47 -0700

    removed spaces

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,30 @@
 #include "main.h"
 
 /**
- * binary_to_uint - Converts a binary number to an unsigned int.
- * @b: A string of 0 and 1 characters.
- * 
- * Return: The converted number, or 0 if there are invalid characters or b is NULL.
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string of 0 and 1 chars
+ * Return: the converted number or 0 if invalid
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    
+    unsigned int num = 0;
+    int i = 0;
+
     if (b == NULL)
-        return (0);
-
-    while (*b)
     {
-        if (*b != '0' && *b != '1')
-            return (0);
-
-        result = (result << 1) | (*b - '0');
-        b++;
+        return (0);
     }
 
-    return (result);
+    while (b[i])
+    {
+        if (b[i] != '0' && b[i] != '1')
+        {
+            return (0);
+        }
+        num = (num << 1) | (b[i] - '0');
+        i++;
+    }
+
+    return (num);
 }
 
