f28715c571f7c286754a8da2dfa088da0ecf8f77
Lance <evanslance100@gmail.com>
2024-08-09 02:58:06 -0700

    updated 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,10 @@
 #include "lists.h"
+#include <unistd.h> /* for write() */
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: Pointer to the head of the list.
+ *
  * Return: The number of nodes.
  */
 size_t print_listint(const listint_t *h)
@@ -11,12 +13,12 @@ size_t print_listint(const listint_t *h)
 
     while (h)
     {
-        _putchar(h->n + '0');  /* Assuming _putchar can handle integers directly */
+        _putchar(h->n + '0'); /* Assuming single digit integers for simplicity */
         _putchar('\n');
         h = h->next;
         count++;
     }
 
-    return (count);
+    return count;
 }
 
