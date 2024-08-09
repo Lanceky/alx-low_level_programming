f7c097402d0081155f6300465b6e947535215e5a
Lance <evanslance100@gmail.com>
2024-08-09 03:36:25 -0700

    removed whitespaces

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,24 +7,24 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int num = 0;
-    int i = 0;
+	unsigned int num = 0;
+	int i = 0;
 
-    if (b == NULL)
-    {
-        return (0);
-    }
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-    while (b[i])
-    {
-        if (b[i] != '0' && b[i] != '1')
-        {
-            return (0);
-        }
-        num = (num << 1) | (b[i] - '0');
-        i++;
-    }
+	while (b[i])
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		num = (num << 1) | (b[i] - '0');
+		i++;
+	}
 
-    return (num);
+	return (num);
 }
 
