07d3c4f9be0458fe5e731f388af8d11a77b65f05
Lance <evanslance100@gmail.com>
2024-09-01 07:03:59 -0700

    initial commit I

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "main.h"
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fd;
+    ssize_t bytes_read, bytes_written;
+    char *buffer;
+
+    if (filename == NULL)
+        return (0);
+
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+    {
+        close(fd);
+        return (0);
+    }
+
+    bytes_read = read(fd, buffer, letters);
+    if (bytes_read == -1)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+    if (bytes_written == -1 || bytes_written != bytes_read)
+    {
+        free(buffer);
+        close(fd);
+        return (0);
+    }
+
+    free(buffer);
+    close(fd);
+    return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,2 @@
+This directry contains projects on file handling, It contains five
+files
