ebfc74948d8a8abc1464bfdf8952bb18e05f2b04
Lance <evanslance100@gmail.com>
2024-09-02 03:12:27 -0700

    deleted duplicate

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * read_textfile - function that reads a text file and prints it to the POSIX standard output.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
  *           function should read and print.
