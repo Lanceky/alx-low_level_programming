c240630c9ce28c92a7311ae767439c48de4f730e
Lanceky <evanslance100@gmail.com>
2024-09-18 09:40:22 -0700

    betty compliance

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,20 +10,20 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new_node;
+dlistint_t *new_node;
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (new_node == NULL)
-        return NULL;
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return NULL;
 
-    new_node->n = n;
-    new_node->prev = NULL;
-    new_node->next = *head;
+new_node->n = n;
+new_node->prev = NULL;
+new_node->next = *head;
 
-    if (*head != NULL)
-        (*head)->prev = new_node;
+if (*head != NULL)
+(*head)->prev = new_node;
 
-    *head = new_node;
+*head = new_node;
 
-    return new_node;
+return (new_node);
 }
