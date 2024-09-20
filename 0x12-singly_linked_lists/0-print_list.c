33116e04d3c0bd9b52c47da02f17cadf2e128277
Lanceky <evanslance100@gmail.com>
2024-09-20 13:28:40 -0700

    refadctored 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_list - prints all elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
- *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
@@ -13,41 +13,12 @@ size_t print_list(const list_t *h)
     while (h != NULL)
     {
         if (h->str == NULL)
-        {
-            _putchar('[');
-            _putchar('0');
-            _putchar(']');
-            _putchar(' ');
-            _putchar('(');
-            _putchar('n');
-            _putchar('i');
-            _putchar('l');
-            _putchar(')');
-        }
+            printf("[0] (nil)\n");
         else
-        {
-            char len_str[11];
-            int len = h->len;
-            int i = 0;
-            if (len == 0)
-                len_str[i++] = '0';
-            while (len > 0)
-            {
-                len_str[i++] = (len % 10) + '0';
-                len /= 10;
-            }
-            len_str[i] = '\0';
-            for (int j = i - 1; j >= 0; j--)
-                _putchar(len_str[j]);
-            _putchar(' ');
-            char *str = h->str;
-            while (*str)
-                _putchar(*str++);
-        }
-        _putchar('\n');
-        count++;
+            printf("[%u] %s\n", h->len, h->str);
         h = h->next;
+        count++;
     }
-    return (count);
-}
 
+    return count;
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -1,1 +1,7 @@
-This directory contains Singly linked lists projects
+This directory contains various projects involving linked lists
+It contians the following files:
+1. lists.h which contians the prototype fucntions used in the rest of the files
+0-print_lists.c
+2-add_node.c
+3-add_node_end.c
+4-free_list.c
