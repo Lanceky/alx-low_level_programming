c50171178c35870d78a5c18e125bc08e53ebb158
Lanceky <evanslance100@gmail.com>
2024-09-20 13:33:11 -0700

    removed spaces at start of lines and added parenthesis to the return value count

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,17 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[0] (nil)\n");
-        else
-            printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
-    }
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
 
-    return count;
+return (count);
 }
