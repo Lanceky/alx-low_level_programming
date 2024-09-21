da855aefaa3feb6faf236cd6a7b1cee036038e55
Lanceky <evanslance100@gmail.com>
2024-09-21 01:14:59 -0700

    refactor 0 and 1 to follw betty codint style: parentheses for return values

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,24 +1,16 @@
 #include "lists.h"
-#include <stddef.h> /* For size_t */
+#include <stdio.h>
 
-/**
- * print_listint - Prints all the elements of a listint_t list.
- * @h: Pointer to the head of the list.
- *
- * Return: The number of nodes.
- */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
+size_t count = 0;
 
-    while (h != NULL)
-    {
-        _putchar('0' + h->n); /* Assuming _putchar is used for single digit printing */
-        _putchar('\n');       /* Print a newline after each number */
-        h = h->next;
-        count++;
-    }
-
-    return (count);
+while (h != NULL)
+{
+printf("%d\n", h->n);
+h = h->next;
+count++;
 }
 
+return (count);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stddef.h> /* For size_t */
+#include <stddef.h>
 
 /**
  * struct listint_s - singly linked list
@@ -12,8 +12,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
@@ -21,9 +21,12 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
-
-/* Prototype for _putchar function */
-int _putchar(char c);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
 #endif /* LISTS_H */
-
