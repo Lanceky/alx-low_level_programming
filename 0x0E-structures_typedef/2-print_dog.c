2ab03d631c1df9e95007fa0d418c5071bb975cad
Lanceky <evanslance100@gmail.com>
2024-09-21 08:17:21 -0700

    added 2-print_dog.c prototypes to header file and removed spaces from 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,10 +11,10 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d != NULL)
-    {
-        printf("Name: %s\n", d->name ? d->name : "(nil)");
-        printf("Age: %f\n", d->age);
-        printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
-    }
+if (d != NUL)
+{
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+}
 }
