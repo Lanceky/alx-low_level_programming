547993eb1ae2a1510b79166d9f7025aa2bc8d563
Lanceky <evanslance100@gmail.com>
2024-09-22 00:16:27 -0700

    removed trailing whitespace from 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - Prints the number of command-line arguments.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- * 
- * Return: Always 0 (Success).
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-(void)argv; /* Ignore unused parameter warning */
+(void)argv;
 printf("%d\n", argc - 1);
 return (0);
 }
-
