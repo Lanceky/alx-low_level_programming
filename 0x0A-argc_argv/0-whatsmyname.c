0b6768c8d68c5f428750301937d1695b8ed1cb98
Lanceky <evanslance100@gmail.com>
2024-09-22 00:05:27 -0700

    removed trailing whitespace from line seven

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - Prints the name of the program.
-* @argc: Number of command-line arguments.
-* @argv: Array of command-line arguments. 
-* Return: Always 0 (Success).
+*main - Prints the name of the program.
+*@argc: Number of command-line arguments.
+*@argv: Array of command-line arguments. 
+*Return: Always 0 (Success).
 */
 int main(int argc, char *argv[])
 {
