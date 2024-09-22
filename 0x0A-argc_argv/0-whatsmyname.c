d0e6f00239139fb68bac74de17c945173b0d7347
Lanceky <evanslance100@gmail.com>
2024-09-22 00:02:08 -0700

    updated 0-whatsmyname.c removed whitespace'

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
- * main - Prints the name of the program.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- * 
- * Return: Always 0 (Success).
- */
+* main - Prints the name of the program.
+* @argc: Number of command-line arguments.
+* @argv: Array of command-line arguments.
+* 
+* Return: Always 0 (Success).
+*/
 int main(int argc, char *argv[])
 {
 (void)argc;  /* Ignore unused parameter warning */
